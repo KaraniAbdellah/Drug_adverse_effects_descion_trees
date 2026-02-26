4fb18748e0b5623b3f89f7dbf08028c93bd2724d
KaraniAbdellah <abdellahkarani@gmail.com>
2026-02-26 12:19:03 +0000

    feat: get data from API and Normalize and try to explozed

--- learn/learn.py
+++ learn/learn.py
@@ -10,16 +10,22 @@ try:
 except:
     file.close()
     print('Error in File Opening')
-
-
 ## With with keyword --> this close file auto
 with open("file.txt") as file:
     content = file.read()
     print(content)
 
 
-
 # DataFrame (is 2 dimentional Object)
-
+# workbook (class in openexcel) is excel file itself
+# Data Wrangling vs Data Cleaning
+'''
+    Data Wrangling OR Data Preprocessing: is preparing the data
+    Data Cleaning: clean data to grant the data quality
+    
+    --> Data Wrangling:
+        - Collect Data From Deffrent Ressources (API, CSV, ...)
+        - 
+'''
 
  
